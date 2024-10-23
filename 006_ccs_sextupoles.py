b38fb4a422f158a949a8f49e514ee0cb287ec571
giadarol <giovanni.iadarola@cern.ch>
2024-10-23 14:29:49 +0200

    Match also the left

--- 006_ccs_sextupoles.py
+++ 006_ccs_sextupoles.py
@@ -9,29 +9,46 @@ env.call('fccee_z_lattice.py')
 env.call('fccee_z_strengths.py')
 
 line = env['fccee_p_ring']
+section = line.select('mid_cell_edge_r::1','mid_cell_edge_l::2')
 cell1 = line.select('mid_cell_edge_l::1','mid_cell_edge_r::1')
+cell2 = line.select('mid_cell_edge_l::2','mid_cell_edge_r::2')
 
 tw_cell_1 = cell1.twiss4d()
 twinit_cell_1_r = tw_cell_1.get_twiss_init('mid_cell_edge_r')
-
-section = line.select('mid_cell_edge_r::1','mid_cell_edge_l::2')
+tw_cell_2 = cell2.twiss4d()
+twinit_cell_2_l = tw_cell_1.get_twiss_init('mid_cell_edge_l')
 
 tw0 = section.twiss(init=twinit_cell_1_r,
                     compute_chromatic_properties=True)
 
 # wipe sextupoles
-for kk in ['ksfx1l', 'ksdy1l', 'ksdm1l', 'ksfm2l']:
+for kk in ['ksfx1l', 'ksdy1l', 'ksdm1l', 'ksfm2l',
+           'ksfx1r', 'ksdy1r', 'ksdm1r', 'ksfm2r',
+           ]:
     env[kk] = 0.001 * np.sign(env[kk])
 
 tw_no_ip_sext = section.twiss(init=twinit_cell_1_r,
                     compute_chromatic_properties=True)
 
-opt1 = section.match(
+opt_w_left = section.match(
+    name='w_left',
     solve=False,
     init=twinit_cell_1_r,
     compute_chromatic_properties=True,
     vary=xt.VaryList(['ksfx1l', 'ksdy1l'], step=1e-4),
     targets=xt.TargetSet(wx_chrom=0, wy_chrom=0, at='ip_mid')
 )
-opt = opt1
+opt = opt_w_left
+opt.step(20)
+
+# Match from right to left
+opt_w_right = section.match(
+    name='w_right',
+    solve=False,
+    init=twinit_cell_2_l,
+    compute_chromatic_properties=True,
+    vary=xt.VaryList(['ksfx1r', 'ksdy1r'], step=1e-4),
+    targets=xt.TargetSet(wx_chrom=0, wy_chrom=0, at='ip_mid')
+)
+opt = opt_w_right
 opt.step(20)
