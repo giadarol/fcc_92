01d4da1d5a10f8935169abe8f5089e618db9af79
giadarol <giovanni.iadarola@cern.ch>
2024-11-03 15:59:17 +0100

    Closed linear optics with my strengths

--- 008_test_together.py
+++ 008_test_together.py
@@ -15,6 +15,7 @@ env.vars.load_json('strengths_quads_02_ffccsxl.json')
 env.vars.load_json('strengths_quads_03_ffccsyr.json')
 env.vars.load_json('strengths_quads_04_ffccsxr.json')
 env.vars.load_json('strengths_quads_05_ffds_lr.json')
+env.vars.load_json('strengths_quads_06_straight.json')
 
 tt = line.get_table(attr=True)
 
@@ -34,16 +35,19 @@ twl = line.twiss(
 )
 
 tw = line.twiss(
-    start='mid_cell_edge_l::1',
+    start='mid_cell_edge_l::0',
     end='mid_cell_edge_r::2',
     init_at='ip_mid::1',
     betx=env['bxip'],
     bety=env['byip'],
 )
 
+tw_on_mom = line.twiss4d(compute_chromatic_properties=False)
+
 import matplotlib.pyplot as plt
 plt.close('all')
 twl.plot()
 twr.plot()
 tw.plot()
+tw_on_mom.plot()
 plt.show()
