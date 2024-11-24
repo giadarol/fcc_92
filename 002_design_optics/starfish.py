5a21970d099252765f11d13f44311b53a355862a
giadarol <giovanni.iadarola@cern.ch>
2024-11-24 15:35:01 +0100

    Add notebook

--- 002_design_optics/starfish.py
+++ 002_design_optics/starfish.py
@@ -37,7 +37,7 @@ class Starfish(xt.Action):
 
 
 
-    def starfish(self, plot=False):
+    def starfish(self, plot=False, figure=None):
 
         line_starfish = self.line_starfish
         tw_sf = self.tw_sf
@@ -65,28 +65,32 @@ class Starfish(xt.Action):
 
         if plot:
             import matplotlib.pyplot as plt
-            plt.figure(figsize=(6.4*2.2, 4.8))
-            plt.subplot(131)
-            plt.plot(ncoord_x.x_norm, ncoord_x.px_norm, '.')
-            plt.axis('equal')
-            plt.xlabel('x')
-            plt.ylabel('px')
-            plt.subplot(132)
-            plt.plot(ncoord_y.y_norm, ncoord_y.py_norm, '.')
-            plt.axis('equal')
-            plt.xlabel('y')
-            plt.ylabel('py')
-            plt.subplot(133)
-            plt.plot(ncoord_xy.x_norm, ncoord_xy.py_norm, '.')
-            plt.axis('equal')
-            plt.xlabel('x=y')
-            plt.ylabel('py')
+            if figure is None:
+                figure = plt.figure(figsize=(6.4*1.3, 4.8*0.6))
+            ax1 = figure.add_subplot(131)
+            ax1.plot(ncoord_x.x_norm, ncoord_x.px_norm, '.')
+            ax1.axis('equal')
+            ax1.set_xlabel(r'$\hat{x}$')
+            ax1.set_ylabel(r'$\hat{p}_x$')
+            ax2 = figure.add_subplot(132)
+            ax2.plot(ncoord_y.y_norm, ncoord_y.py_norm, '.')
+            ax2.axis('equal')
+            ax2.set_xlabel(r'$\hat{y}$')
+            ax2.set_ylabel(r'$\hat{p}_y$')
+            ax3 = figure.add_subplot(133)
+            ax3.plot(ncoord_xy.x_norm, ncoord_xy.py_norm, '.')
+            ax3.axis('equal')
+            ax3.set_xlabel(r'$\hat{x}\simeq \hat{y}$')
+            ax3.set_ylabel(r'$\hat{p}_y$')
+            figure.subplots_adjust(left=0.1, bottom=0.2, right=0.97, wspace=0.5)
 
         out ={'ncoord_x': ncoord_x._data, 'ncoord_y': ncoord_y._data, 'ncoord_xy': ncoord_xy._data}
 
         px_norm_rms_5 = ncoord_x.px_norm[:, 5].std()
+        py_norm_rms_5 = ncoord_y.py_norm[:, 5].std()
         pxy_norm_rms_5 = ncoord_xy.py_norm[:, 5].std()
         out['px_norm_rms_5'] = px_norm_rms_5
+        out['py_norm_rms_5'] = py_norm_rms_5
         out['pxy_norm_rms_5'] = pxy_norm_rms_5
 
         return out
