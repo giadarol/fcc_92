05bec9957032c42cbb25d5c8278429b5a66c94f9
giadarol <giovanni.iadarola@cern.ch>
2025-01-25 10:40:03 +0100

    This works!

--- 001_characterize_lattice/002_madng.py
+++ 001_characterize_lattice/002_madng.py
@@ -12,8 +12,18 @@ env["kdecdr"] = env["kdecdr"] # Set by value
 env["ksdm1l"] = env["ksdm1l"] # Set by value
 env["ksdm1r"] = env["ksdm1r"] # Set by value
 
-line = env.fccee_p_ring
+line = env.fccee_p_ring.copy(shallow=True)
+line.replace_all_repeated_elements()
+line.build_madng_model()
 
 tw = line.twiss4d()
 
-mng = line.to_madng(keep_files=True, temp_fname='ttt', mode='line')
+mng = line.to_madng(sequence_name='seq')#, temp_fname='ttt', keep_files=True)
+
+mng.send('''
+local damap in MAD
+local seq = MADX.seq
+''')
+
+mng["mytwtable", 'mytwflow'] = mng.twiss(
+    sequence=mng.seq, method=4, mapdef=2, implicit=True, nslice=3, save="'atbody'")
